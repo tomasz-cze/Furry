014bf75edb257ec1316f80c8191a93f5e952787e
Tomasz Czechlewski <tomasz.czechlewski@gmail.com>
2018-06-28 18:51:31 +0200

    added function to show and hide coin, and function to change direction and move furry in right direction

--- js/game.js
+++ js/game.js
@@ -37,4 +37,49 @@ function Game() {
         }, 250);
     };
 
+    //function showing coin in new place
+
+    this.showCoin = function () {
+        this.board[this.index(this.coin.x, this.coin.y)].classList.add('coin');
+    };
+
+    this.hideVisibleCoin = function () {
+        if (this.furry.x == this.coin.x &&  this.furry.y == this.coin.y) {
+            document.querySelector(".coin").classList.remove("coin");
+        }
+    };
+
+    // function changing furry move
+
+    this.turnFurry = function () {
+        switch (event.which) {
+            case 37:
+                self.furry.direction = "left";
+                break;
+            case 39:
+                self.furry.direction = "right";
+                break;
+            case 40:
+                self.furry.direction = "up";
+                break;
+            case 38:
+                self.furry.direction = "down";
+                break;
+        }
+    };
+
+    this.moveFurry = function () {
+        if (this.furry.direction == "right") {
+            this.furry.x = this.furry.x + 1;
+        } else if (this.furry.direction == "left") {
+            this.furry.x = this.furry.x - 1;
+        } else if (this.furry.direction == "up") {
+            this.furry.y = this.furry.y + 1;
+        } else if (this.furry.direction == "down") {
+            this.furry.y = this.furry.y - 1;
+        }
+
+    };
+
+
 }
